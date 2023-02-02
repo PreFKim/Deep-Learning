64fd951b18c7d104d7df4204e6a65f9f8f56ff57
PreFKim <pre_f_86@naver.com>
2023-02-02 18:13:37 +0900

    Update LeNet code

--- Classification/01.LeNet-5/LeNet-5(keras).py
+++ Classification/01.LeNet-5/LeNet-5(keras).py
@@ -6,18 +6,14 @@ def lenet_5():
   C1 = keras.layers.Conv2D(6,5)(x)
   C1 = keras.activations.relu(C1)
 
-  S2 = keras.layers.AveragePooling2D(2,2,'same')(C1)
+  S2 = keras.layers.AveragePooling2D(2,2)(C1)
 
   C3 = keras.layers.Conv2D(16,5)(S2)
   C3 = keras.activations.relu(C3)
 
-  S4 = keras.layers.AveragePooling2D(2,2,'same')(C3)
+  S4 = keras.layers.AveragePooling2D(2,2)(C3)
   
   C5 = keras.layers.Conv2D(120,5)(S4)
-  
-  #F = keras.layers.Flatten()(S4)
-  #C5 = keras.layers.Dense(120)(F)
-
   C5 = keras.activations.relu(C5)
 
   F = keras.layers.Flatten()(C5)
--- Classification/01.LeNet-5/LeNet-5(pytorch).py
+++ Classification/01.LeNet-5/LeNet-5(pytorch).py
@@ -1,19 +1,20 @@
 import torch.nn as nn
+from torchsummary import summary
 
 class LeNet_5(nn.Module):
     def __init__(self):
         super(LeNet_5,self).__init__()
 
         self.c1 = nn.Sequential(
-            nn.Conv2d(3,6,5,1,0),
+            nn.Conv2d(1,6,5,1,0),
             nn.ReLU()
         )
-        self.s2 = nn.AvgPool2d(2,2,1)
+        self.s2 = nn.AvgPool2d(2,2,0)
         self.c3 = nn.Sequential(
             nn.Conv2d(6,16,5,1,0),
             nn.ReLU()
         )
-        self.s4 = nn.AvgPool2d(2,2,1)
+        self.s4 = nn.AvgPool2d(2,2,0)
 
         self.c5 = nn.Sequential(
             nn.Conv2d(16,120,5,1,0),
@@ -38,8 +39,9 @@ class LeNet_5(nn.Module):
         c3 = self.c3(s2)
         s4 = self.s4(c3)
         c5 = self.c5(s4)
+        c5 = c5.view(-1,c5.size(1))
         fc1 = self.fc1(c5)
         fc2 = self.fc2(fc1)
 
         return fc2
-model = LeNet_5()
+summary(LeNet_5(),(1,32,32))
