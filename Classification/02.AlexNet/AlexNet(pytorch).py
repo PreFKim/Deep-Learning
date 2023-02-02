146c336d60b8a3e54a9b387108f2aa6405dbca49
PreFKim <pre_f_86@naver.com>
2023-02-02 18:21:40 +0900

    Update AlexNet(pytorch).py

--- Classification/02.AlexNet/AlexNet(pytorch).py
+++ Classification/02.AlexNet/AlexNet(pytorch).py
@@ -1,37 +1,38 @@
 import torch.nn as nn
+from torchsummary import summary
 
 class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet,self).__init__()
 
         self.l1 = nn.Sequential(
-            nn.Conv2d(3,96,11,4,0),
+            nn.Conv2d(3,96,11,4),
             nn.ReLU()
         )
         self.l2 = nn.Sequential(
             nn.BatchNorm2d(96),
             nn.MaxPool2d(3,2),
-            nn.Conv2d(96,256,5,1,1),
+            nn.Conv2d(96,256,5,1,'same'),
             nn.ReLU()
         )
         self.l3 = nn.Sequential(
             nn.BatchNorm2d(256),
             nn.MaxPool2d(3,2),
-            nn.Conv2d(256,384,3,1,1),
+            nn.Conv2d(256,384,3,1,'same'),
             nn.ReLU()
         )
         self.l4 = nn.Sequential(
-            nn.Conv2d(384,384,3,1,1),
+            nn.Conv2d(384,384,3,1,'same'),
             nn.ReLU()
         )
 
         self.l5 = nn.Sequential(
-            nn.Conv2d(384,256,3,1,1),
+            nn.Conv2d(384,256,3,1,'same'),
             nn.ReLU()
         )
+        self.pool = nn.AdaptiveAvgPool2d(1)
 
         self.fc1 = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
             nn.Linear(256,4096),
             nn.ReLU(),
             nn.Dropout(0.5)
@@ -54,10 +55,13 @@ class AlexNet(nn.Module):
         l4 = self.l4(l3)
         l5 = self.l5(l4)
 
-        fc1 = self.fc1(l5)
+        pool = self.pool(l5)
+        pool = pool.view(-1,pool.size(1))
+
+        fc1 = self.fc1(pool)
         fc2 = self.fc2(fc1)
         output = self.output(fc2)
 
         return output
 
-model = AlexNet()
+summary(AlexNet(),(3,227,227))
