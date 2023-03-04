a2f6d9a84ce2262ab40b77fdf84e2d88a5ac88c3
PreFKim <pre_f_86@naver.com>
2023-03-04 16:55:20 +0900

    Update loss(keras).py

--- Paper review(CV)/Object-Detection/01.YOLO/loss(keras).py
+++ Paper review(CV)/Object-Detection/01.YOLO/loss(keras).py
@@ -1,74 +1,61 @@
 import keras.backend as K
 
-def loss_function(y_true,y_pred):
-    #y_true,y_pred 모두 ([B,W,H,C])로 들어온다.
+def loss_function(y_true,y_pred,lambda_coord=5,lambda_noobj=0.5):
     #이때 y_true 는 (s,s,5+c)로 들어오고 y pred는 (s,s,b*5+c)로 들어온다.
-    #x,y,w,h,confidence
-
-    lambda_coord = 5
-    lambda_noobj = 0.5
+    #(x,y,w,h,confidence)*b,classes(c)
 
     batch_size = y_true.shape[0]
     s = y_true.shape[1]
-    b = int((y_pred.shape[-1] - y_true.shape[-1]) / 5 + 1)
-    c = int(y_true.shape[-1] - (b-1)*5)
-    print(s,b,c)
+    b = (y_pred.shape[-1] - y_true.shape[-1]) // 5 + 1
+    c = y_true.shape[-1] - (b-1)*5
 
     y_true_f = K.reshape(y_true,(-1,s*s,5+c))
     y_pred_f = K.reshape(y_pred,(-1,s*s,b*5+c))
-    loss = []
-    for i in range(batch_size):
 
-        cell_loss = []
-        for j in range(s*s):
-            is_exist = y_true_f[i,j,4]
-            # Responsible 구하기
-            iou_list = []
-            for k in range(b):
-                xt,yt,wt,ht,ct = y_true_f[i,j,0:5]
-                xp,yp,wp,hp,cp = y_pred_f[i,j,k*5:k*5+5]
-                
-                x1t,y1t,x2t,y2t = [xt-0.5*wt,yt-0.5*ht,xt+0.5*wt,yt+0.5*ht]
-                x1p,y1p,x2p,y2p = [xp-0.5*wp,yp-0.5*hp,xp+0.5*wp,yp+0.5*hp]
-                
-                x1i,y1i,x2i,y2i = [max(x1t,x1p),max(y1t,y1p),min(x2t,x2p),min(y2t,y2p)]
+    true_conf = K.reshape(y_true_f[:,:,4],(-1,s*s,1,1))
+    true_xy = K.expand_dims(y_true_f[:,:,:2],2) # -1,S*S,1,2
+    true_wh = K.expand_dims(y_true_f[:,:,2:4],2) # -1,S*S,1,2
+    true_class = y_true_f[:,:,5:] # -1,S*S,C
 
-                intersection = 0
-                if x2i - x1i >= 0 and y2i - y1i >= 0:
-                    intersection = (x2i-x1i) * (y2i-y1i) 
+    pred_box = K.reshape(y_pred_f[:,:,:b*5],(-1,s*s,b,5))
+    pred_conf = K.expand_dims(pred_box[:,:,:,4],-1) # -1,S*S,B,1
+    pred_xy = pred_box[:,:,:,:2] # -1,S*S,B,2
+    pred_wh = pred_box[:,:,:,2:4] # -1,S*S,B,2
+    pred_class = y_pred_f[:,:,b*5:] # -1,S*S,C
 
 
-                union = (x2t-x1t) * (y2t-y1t) + (x2p-x1p) * (y2p-y1p) - intersection
+    #responsible 구하기
+    true_min_xy = true_xy-true_wh/2
+    true_max_xy = true_xy+true_wh/2
 
-                if union == 0:
-                    iou_list.append(0)
-                else :
-                    iou_list.append(intersection/union)
+    pred_min_xy = pred_xy-pred_wh/2
+    pred_max_xy = pred_xy+pred_wh/2
 
-            responsible = K.argmax(iou_list)*5
 
-            localization1 = K.pow(y_true_f[i,j,0]-y_pred_f[i,j,responsible],2) + K.pow(y_true_f[i,j,1]-y_pred_f[i,j,responsible+1],2)
-            localization2 = K.pow(K.sqrt(y_true_f[i,j,2])-K.sqrt(y_pred_f[i,j,responsible+2]),2) + K.pow(K.sqrt(y_true_f[i,j,3])-K.sqrt(y_pred_f[i,j,responsible+3]),2)
+    inter_min_xy = K.maximum(true_min_xy,pred_min_xy)
+    inter_max_xy = K.minimum(true_max_xy,pred_max_xy)
 
-            localization = lambda_coord * (localization1+localization2)
+    inter_area = K.maximum(inter_max_xy-inter_min_xy,0)
+    true_area = true_max_xy - true_min_xy
+    pred_area = pred_max_xy - pred_min_xy
 
-            
-            confidence = 0
-            if is_exist:
-                #객체가 있는 경우에는 responsible만 이용해 loss구하기
-                confidence = K.pow(y_true_f[i,j,4]-y_pred_f[i,j,responsible+4],2)
-            else :
-                #객체가 없는 경우에는 Bounding box를 이용해 loss구하기
-                for k in range(b):
-                    confidence += K.pow(y_true_f[i,j,4]-y_pred_f[i,j,k*5+4],2)
-                confidence *= lambda_noobj
-            
-            classification = 0
-            for k in range(c):
-                classification+= K.pow(y_true_f[i,j,5+k] - y_pred_f[i,j,b*5+k],2)
-            
+    intersection = inter_area[:,:,:,0] * inter_area[:,:,:,1] 
+    
+    union = true_area[:,:,:,0]*true_area[:,:,:,1] + pred_area[:,:,:,0]*pred_area[:,:,:,1] - intersection
 
-            cell_loss.append(localization+confidence+classification)
-        loss.append(sum(cell_loss)/s**2)
-    ret = sum(loss)/batch_size
-    return ret
+    ious = intersection / union # -1,S*S,B
+
+    best_iou = K.max(ious,-1,True) # -1,S*S,1
+
+    responsible = K.expand_dims(K.cast(ious >= best_iou,dtype=ious.dtype),-1) # -1,S*S,B,1
+
+    #최종 loss 구하기
+    localization1 = K.sum(K.pow(true_xy-pred_xy,2)*responsible*true_conf) 
+    localization2 = K.sum(K.pow(K.sqrt(true_xy)-K.sqrt(pred_xy),2)*responsible*true_conf)
+    conf_obj = K.sum(K.pow(true_conf-pred_conf,2)*responsible*true_conf)
+    conf_no_obj = K.sum(K.pow(true_conf-pred_conf,2)*(1-responsible*true_conf))
+    classification = K.sum(K.pow(pred_class - true_class,2)*K.reshape(true_conf,(-1,s*s,1)))
+
+    loss = lambda_coord*(localization1+localization2)+conf_obj+lambda_noobj * conf_no_obj + classification
+
+    return loss 
